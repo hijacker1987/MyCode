5691f0b893b3ffab93db6ea214a64e3a63f8614b
K Balázs <kutbal87@gmail.com>
2024-01-10 08:42:08 +0100

    DB Context added + necessary deps

---
+++ MyCode Backend Server/MyCode Backend Server/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyCode_Backend_Server.Models;
+
+namespace MyCode_Backend_Server.Data
+{
+    public class DataContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+    {
+        public DbSet<User>? UsersDb { get; set; }
+        public DbSet<Code>? CodesDb { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DataContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- MyCode Backend Server/MyCode Backend Server/Startup.cs
+++ MyCode Backend Server/MyCode Backend Server/Startup.cs
@@ -1,14 +1,21 @@
-﻿namespace MyCode_Backend_Server
+﻿using Microsoft.EntityFrameworkCore;
+using MyCode_Backend_Server.Data;
+
+namespace MyCode_Backend_Server
 {
     public class Startup(IConfiguration configuration)
     {
-        public IConfiguration Configuration { get; } = configuration;
+        private readonly IConfiguration _configuration = configuration;
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
+
+            var connection = _configuration["ConnectionString"];
+
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
