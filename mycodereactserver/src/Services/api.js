aa44754ac3caccc04712e82f99b688868a6e7a5d
K Balázs <kutbal87@gmail.com>
2024-01-16 15:46:12 +0100

    unnecessary console.error duplications removed (exactly from api.js)

--- mycodereactserver/src/Services/api.js
+++ mycodereactserver/src/Services/api.js
@@ -1,48 +1,26 @@
 import { backendUrl } from '../Services/config';
 
 export const postApi = async (user, endpoint) => {
-    try {
-        const response = await fetch(`${backendUrl}${endpoint}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(user),
-        });
+    const response = await fetch(`${backendUrl}${endpoint}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+    });
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        return data;
-    } catch (error) {
-        console.error(`Error occurred during ${endpoint} request:`, error);
-        throw error;
-    }
+    return response.json();
 };
 
 export const patchApi = async (user, token, endpoint) => {
-    try {
-        const response = await fetch(`${backendUrl}${endpoint}`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            },
-            body: JSON.stringify(user)
-        });
+    const response = await fetch(`${backendUrl}${endpoint}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(user)
+    });
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        return data;
-    } catch (error) {
-        console.error(`Error occurred during ${endpoint} request:`, error);
-        throw error;
-    }
+    return response.json();
 };
