c3df3a2879c6e3ae343ae1306da514959b3a02c6
K Balázs <kutbal87@gmail.com>
2024-03-07 11:46:19 +0100

    Add model to 2fa, micromanagement to 2fa

--- MyCode Backend Server/MyCode Backend Server/Controllers/AuthController.cs
+++ MyCode Backend Server/MyCode Backend Server/Controllers/AuthController.cs
@@ -18,6 +18,34 @@ namespace MyCode_Backend_Server.Controllers
         private readonly IEmailSender _emailSender = emailSender;
         private readonly ILogger<TokenController> _logger = logger;
 
+        [HttpGet("basicsTwoFactor"), Authorize(Roles = "Admin, User")]
+        public ActionResult BasicsTwoFactor(string userId)
+        {
+            var authorizationCookie = Request.Cookies["Authorization"];
+
+            if (_tokenService.ValidateToken(authorizationCookie!))
+            {
+                var checkedToken = _tokenService.Refresh(authorizationCookie!, Request, Response);
+
+                if (checkedToken == null)
+                {
+                    _logger.LogError("Token expired.");
+                    return BadRequest("Token expired.");
+                }
+            }
+
+            if (string.IsNullOrEmpty(userId))
+                return BadRequest("Must have an Id");
+
+            var user = _dataContext.Users.FirstOrDefault(u => u.Id.ToString() == userId);
+
+            if (user == null)
+                return NotFound();
+
+            var basicInfo = new { user.TwoFactorEnabled, user.EmailConfirmed };
+
+            return CreatedAtAction("BasicsTwoFactor", basicInfo);
+        }
 
         [HttpPost("enableTwoFactor"), Authorize(Roles = "Admin, User")]
         public async Task<ActionResult> EnableTwoFactor([FromBody] string userId)
@@ -35,42 +63,65 @@ namespace MyCode_Backend_Server.Controllers
                 }
             }
 
+            if (string.IsNullOrEmpty(userId))
+                return BadRequest("Must have an Id");
+
             var user = _dataContext.Users.FirstOrDefault(u => u.Id.ToString() == userId);
+
             if (user == null)
                 return NotFound();
+
+            if (user.EmailConfirmed)
+                return BadRequest("The security option already set!");
+
+            if (user.TwoFactorEnabled)
+                return BadRequest("Already enabled, ask for verification E-mail!");
 
             await _userManager.SetTwoFactorEnabledAsync(user, true);
 
             var code = await _userManager.GenerateTwoFactorTokenAsync(user, "Email");
-            await _emailSender.SendEmailAsync(user.Email!, "Activate two factor authentication", $"Your unique code for activation: {code}");
-            _logger.LogInformation("Email sent with the code for 2fa.");
+
+            try
+            {
+                await _emailSender.SendEmailAsync(user.Email!, "Activate two factor authentication", $"Your unique code for activation: {code}");
+                _logger.LogInformation("Email sent with the code for 2fa.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error sending email: {ex.Message}");
+                return StatusCode(500, "Error sending email. Please try again later.");
+            }
 
             return Ok();
         }
 
         [HttpPost("verifyTwoFactor"), Authorize(Roles = "Admin, User")]
-        public async Task<ActionResult> VerifyTwoFactor([FromBody] string userId, string code)
+        public async Task<ActionResult> VerifyTwoFactor([FromBody] VerifyModel verify)
         {
-            var authorizationCookie = Request.Cookies["Authorization"];
+            if (string.IsNullOrEmpty(verify.UserId))
+                return BadRequest("Must provide a user Id");
 
-            if (_tokenService.ValidateToken(authorizationCookie!))
-            {
-                var checkedToken = _tokenService.Refresh(authorizationCookie!, Request, Response);
+            if (string.IsNullOrEmpty(verify.Code))
+                return BadRequest("Must provide a verification code");
 
-                if (checkedToken == null)
-                {
-                    _logger.LogError("Token expired.");
-                    return BadRequest("Token expired.");
-                }
-            }
+            var user = _dataContext.Users.FirstOrDefault(u => u.Id.ToString() == verify.UserId);
 
-            var user = _dataContext.Users.FirstOrDefault(u => u.Id.ToString() == userId);
             if (user == null)
-                return NotFound();
+                return NotFound("User not found");
 
-            var isValid = await _userManager.VerifyTwoFactorTokenAsync(user, "Email", code);
+            if (!user.TwoFactorEnabled)
+                return BadRequest("Two-factor authentication is not enabled for this user");
+
+            if (user.EmailConfirmed)
+                return BadRequest("Two-factor authentication is already confirmed for this user");
+
+            var isValid = await _userManager.VerifyTwoFactorTokenAsync(user, "Email", verify.Code);
+
             if (!isValid)
-                return BadRequest("Code doesn't match.");
+                return BadRequest("Invalid verification code");
+
+            user.EmailConfirmed = true;
+            await _dataContext.SaveChangesAsync();
 
             _logger.LogInformation("Email verified with the code for 2fa.");
 
@@ -93,12 +144,19 @@ namespace MyCode_Backend_Server.Controllers
                 }
             }
 
+            if (string.IsNullOrEmpty(userId))
+                return BadRequest("Must have an Id");
+
             var user = _dataContext.Users.FirstOrDefault(u => u.Id.ToString() == userId);
+
             if (user == null)
                 return NotFound();
 
             await _userManager.SetTwoFactorEnabledAsync(user, false);
             _logger.LogInformation($"{user.UserName} disabled 2fa.");
+
+            user.EmailConfirmed = false;
+            await _dataContext.SaveChangesAsync();
 
             return Ok();
         }
---
+++ MyCode Backend Server/MyCode Backend Server/Models/VerifyModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MyCode_Backend_Server.Models
+{
+    public class VerifyModel
+    {
+        public required string UserId { get; set; }
+        public required string Code { get; set; }
+    }
+}
