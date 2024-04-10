a51e5f26ad5f0f30083b22b2fb17f42dc6d0b6a0
K. Balázs <kutbal87@gmail.com>
2024-04-10 14:04:44 +0200

    Add github login + major refactor

--- MyCode Backend Server/MyCode Backend Server/Controllers/OathController.cs
+++ MyCode Backend Server/MyCode Backend Server/Controllers/OathController.cs
@@ -7,7 +7,7 @@ using MyCode_Backend_Server.Service.Authentication;
 using Microsoft.AspNetCore.Identity;
 using MyCode_Backend_Server.Models;
 using Microsoft.AspNetCore.Authentication.Facebook;
-using MyCode_Backend_Server.Data;
+using AspNet.Security.OAuth.GitHub;
 
 namespace MyCode_Backend_Server.Controllers
 {
@@ -30,50 +30,7 @@ namespace MyCode_Backend_Server.Controllers
         {
             var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
-            if (result?.Principal != null)
-            {
-                var claims = result.Principal!.Identities.FirstOrDefault()!.Claims.Select(claim => new
-                {
-                    claim.Issuer,
-                    claim.OriginalIssuer,
-                    claim.Type,
-                    claim.Value
-                });
-
-                var email = claims.ElementAtOrDefault(4)?.Value!;
-                var username = claims.ElementAtOrDefault(0)?.Value!;
-                var displayName = claims.ElementAtOrDefault(1)?.Value!;
-
-                var loginResult = await _authenticationService.LoginExternalAsync(email, Request, Response);
-
-                if (!loginResult.Success)
-                {
-                    var regPass = await _authenticationService.RegisterExternalAccAsync(email, username, displayName, "Googler");
-
-                    loginResult = await _authenticationService.LoginAccAsync(email, regPass, regPass, Request, Response);
-                }
-
-                if (loginResult.Success)
-                {
-                    var managedUser = await _authenticationService.TryGetUser(email);
-                    if (managedUser == null)
-                    {
-                        return NotFound("User not found.");
-                    }
-
-                    await _authenticationService.ApprovedAccLogin(managedUser, Request, Response);
-
-                    return Redirect($"https://localhost:5173/myCodeHome/");
-                }
-                else
-                {
-                    return BadRequest("Failed to login user");
-                }
-            }
-            else
-            {
-                return BadRequest("Authentication failed");
-            }
+            return result?.Principal != null ? await ExternalLoginHelper(result, [4, 0, 1], "Google user") : BadRequest("Authentication failed");
         }
 
         [AllowAnonymous]
@@ -87,49 +44,61 @@ namespace MyCode_Backend_Server.Controllers
         {
             var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
-            if (result?.Principal != null)
+            return result?.Principal != null ? await ExternalLoginHelper(result, [1, 0, 2], "Facebook user") : BadRequest("Authentication failed");
+        }
+
+        [AllowAnonymous]
+        [HttpGet("github-login")]
+        public async Task GitHubLogin() => await HttpContext.ChallengeAsync(GitHubAuthenticationDefaults.AuthenticationScheme,
+                new AuthenticationProperties { RedirectUri = Url.Action(nameof(GitHubResponse)) });
+
+        [AllowAnonymous]
+        [HttpGet("github-response")]
+        public async Task<IActionResult> GitHubResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return result?.Principal != null ? await ExternalLoginHelper(result, [7, 2, 1], "GitHub user") : BadRequest("Authentication failed");
+        }
+
+        private async Task<IActionResult> ExternalLoginHelper(AuthenticateResult result, int[] query, string externalUse)
+        {
+            var claims = result.Principal!.Identities.FirstOrDefault()!.Claims.Select(claim => new
             {
-                var claims = result.Principal!.Identities.FirstOrDefault()!.Claims.Select(claim => new
+                claim.Issuer,
+                claim.OriginalIssuer,
+                claim.Type,
+                claim.Value
+            });
+
+            var email = claims.ElementAtOrDefault(query[0])?.Value!;
+            var username = claims.ElementAtOrDefault(query[1])?.Value!;
+            var displayName = claims.ElementAtOrDefault(query[2])?.Value!;
+
+            var loginResult = await _authenticationService.LoginExternalAsync(email, Request, Response);
+
+            if (!loginResult.Success)
+            {
+                var regPass = await _authenticationService.RegisterExternalAccAsync(email, username, displayName, externalUse);
+
+                loginResult = await _authenticationService.LoginAccAsync(email, regPass, regPass, Request, Response);
+            }
+
+            if (loginResult.Success)
+            {
+                var managedUser = await _authenticationService.TryGetUser(email);
+                if (managedUser == null)
                 {
-                    claim.Issuer,
-                    claim.OriginalIssuer,
-                    claim.Type,
-                    claim.Value
-                });
-
-                var email = claims.ElementAtOrDefault(1)?.Value!;
-                var username = claims.ElementAtOrDefault(0)?.Value!;
-                var displayName = claims.ElementAtOrDefault(2)?.Value!;
-                
-                var loginResult = await _authenticationService.LoginExternalAsync(email, Request, Response);
-
-                if (!loginResult.Success)
-                {
-                    var regPass = await _authenticationService.RegisterExternalAccAsync(email, username, displayName, "Facebook user");
-
-                    loginResult = await _authenticationService.LoginAccAsync(email, regPass, regPass, Request, Response);
+                    return NotFound("User not found.");
                 }
 
-                if (loginResult.Success)
-                {
-                    var managedUser = await _authenticationService.TryGetUser(email);
-                    if (managedUser == null)
-                    {
-                        return NotFound("User not found.");
-                    }
+                await _authenticationService.ApprovedAccLogin(managedUser, Request, Response);
 
-                    await _authenticationService.ApprovedAccLogin(managedUser, Request, Response);
-
-                    return Redirect($"https://localhost:5173/myCodeHome/");
-                }
-                else
-                {
-                    return BadRequest("Failed to login user");
-                }
+                return Redirect($"https://localhost:5173/myCodeHome/");
             }
             else
             {
-                return BadRequest("Authentication failed");
+                return BadRequest("Failed to login user");
             }
         }
     }
