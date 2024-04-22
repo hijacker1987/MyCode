6122f58c97043bc5ca3d04c5438c9dbfc34e280e
K. Balázs <kutbal87@gmail.com>
2024-04-22 17:10:55 +0200

    Remove dummy data to separate file, Add basic questions for bot

--- MyCode Backend Server/MyCode Backend Server/Data/IDbInitializer.cs
+++ MyCode Backend Server/MyCode Backend Server/Data/IDbInitializer.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using MyCode_Backend_Server.Data.Service;
 using MyCode_Backend_Server.Models;
 
 namespace MyCode_Backend_Server.Data
 {
     public interface IDbInitializer
     {
-        Task Initialize(DataContext context, UserManager<User> userManager, RoleManager<IdentityRole<Guid>> roleManager);
+        Task Initialize(DataContext context, UserManager<User> userManager, RoleManager<IdentityRole<Guid>> roleManager, FAQBotData fAQBotData, DummyData dummyData);
     }
 }
---
+++ MyCode Backend Server/MyCode Backend Server/Data/Service/DummyData.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MyCode_Backend_Server.Models;
+
+namespace MyCode_Backend_Server.Data.Service
+{
+    public class DummyData
+    {
+        public async Task InitializeDummyDataAsync(UserManager<User> userManager, DataContext context)
+        {
+            var random = new Random();
+            var roleList = new List<string> { "Admin", "User" };
+            var userNames = new List<string>() { "John Doe", "Jane Doe" };
+            var codeTypes = new List<string>() { "Batch", "C", "C#", "C++", "CoffeeScript", "CSS", "Diff", "Elm", "F#", "Go",
+                                                 "Handlebars", "Haskell", "HTML", "Java", "JavaScript", "JSON", "Kotlin", "LESS", "Lua", "Markdown",
+                                                 "MATLAB", "Objective-C", "Perl", "PHP", "Powershell", "Pug", "Python", "R", "Razor", "Ruby",
+                                                 "Rust", "SASS", "Scala", "SCSS", "Shell", "Swift", "TypeScript", "Turbo Pascal", "VB", "XML" };
+
+            var users = Enumerable.Range(1, 30).Select(i => new User
+            {
+                UserName = $"User{i}",
+                Email = $"user{i}@example.com",
+                DisplayName = $"{i}. example {userNames[random.Next(userNames.Count)]}",
+                PhoneNumber = "123"
+            }).ToArray();
+
+            foreach (var user in users)
+            {
+                var userResult = await userManager.CreateAsync(user, "Password");
+
+                if (userResult.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "User");
+                }
+            }
+
+            var gUser = new User
+            {
+                UserName = "GoogleUser",
+                Email = "googleuser@gmail.com",
+                DisplayName = "Googler",
+                PhoneNumber = "123456",
+                TwoFactorEnabled = true,
+                EmailConfirmed = true
+            };
+
+            var gUserResult = await userManager.CreateAsync(gUser, "Password");
+
+            if (gUserResult.Succeeded)
+            {
+                await userManager.AddToRoleAsync(gUser, "User");
+            }
+
+            var mfaResult = new Mfa
+            {
+                UserId = gUser.Id,
+                ReliableEmail = "googleuser@gmail.com",
+                SecondaryLoginMethod = true,
+            };
+
+            await context.MFADb!.AddAsync(mfaResult);
+
+            foreach (var user in users)
+            {
+                int numberOfCodes = random.Next(1, 6);
+
+                for (int i = 1; i <= numberOfCodes; i++)
+                {
+                    var code = new Code
+                    {
+                        CodeTitle = $"Code of {user.UserName} - {i}",
+                        MyCode = $"CodeContent{i}",
+                        WhatKindOfCode = $"{codeTypes[random.Next(codeTypes.Count)]}",
+                        IsBackend = random.Next(2) == 0,
+                        IsVisible = random.Next(3) == 0,
+                        UserId = user.Id
+                    };
+
+                    context.CodesDb!.Add(code);
+                }
+            }
+            context.SaveChanges();
+        }
+    }
+}
