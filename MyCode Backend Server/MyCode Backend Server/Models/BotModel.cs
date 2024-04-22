6c6ed805db016816bee0d32cf77b62f2275f9dba
K. Balázs <kutbal87@gmail.com>
2024-04-22 17:13:04 +0200

    Add adjustments for previous changes

---
+++ MyCode Backend Server/MyCode Backend Server/Models/BotModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCode_Backend_Server.Models
+{
+    public class BotModel
+    {
+        [Key]
+        public Guid BotId { get; set; }
+        public string? Question { get; set; }
+        public string? Answer { get; set; }
+
+        public BotModel(string question, string answer)
+        {
+            Question = question;
+            Answer = answer;
+        }
+
+        public BotModel()
+        {
+            BotId = Guid.NewGuid();
+        }
+    }
+}
