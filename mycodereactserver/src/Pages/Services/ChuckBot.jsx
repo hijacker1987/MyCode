8a77e9a204e112774b56cc099c158ca136c7384b
K. Balázs <kutbal87@gmail.com>
2024-04-28 10:06:06 +0200

    Add ability to "type", more dynamic and user friendly display

--- mycodereactserver/src/Pages/Services/ChuckBot.jsx
+++ mycodereactserver/src/Pages/Services/ChuckBot.jsx
@@ -1,16 +1,29 @@
 import React, { useState, useRef, useEffect } from "react";
 
+import { useUser } from "../../Services/UserContext";
+
 import "../../Components/Styles/CustomCss/ChuckBot.css";
 import "../../Components/Styles/CustomCss/CBButton.css";
 
 const ChuckBot = () => {
     const outputRef = useRef(null);
+    const { userData } = useUser();
+    const { username } = userData;
+    const [displayName, setDisplayName] = useState("Guest");
     const [messages, setMessages] = useState([]);
     const [inputText, setInputText] = useState("");
+    const [isTyping, setIsTyping] = useState(false);
     const [botOpen, setBotOpen] = useState(false);
 
     const botName = "Chat Norris";
-    const userName = "Quest";
+
+    useEffect(() => {
+        if (username !== "" && username != undefined && username != null) {
+            setDisplayName(username);
+        } else {
+            setDisplayName("Guest");
+        }
+    }, [username]);
 
     const toggleBot = () => {
         setBotOpen(!botOpen);
@@ -18,7 +31,15 @@ const ChuckBot = () => {
 
     const handleMessageSend = async () => {
         const newMessage = { text: inputText };
+        setIsTyping(true);
+
         try {
+            setMessages(prevMessages => [
+                ...prevMessages,
+                { text: `${displayName}: ${inputText}`, mine: true },
+            ]);
+            setInputText("");
+
             const response = await fetch(`https://localhost:7001/cncbot/messages`, {
                 method: "POST",
                 headers: {
@@ -29,12 +50,13 @@ const ChuckBot = () => {
 
             const responseData = await response.json();
 
-            setMessages(prevMessages => [
-                ...prevMessages,
-                { text: `${userName}: ${inputText}`, mine: true },
-                { text: `${botName}: ${responseData}`, mine: false }
-            ]);
-            setInputText("");
+            setTimeout(() => {
+                setMessages(prevMessages => [
+                    ...prevMessages,
+                    { text: `${botName}: ${responseData}`, mine: false }
+                ]);
+                setIsTyping(false);
+            }, 2000);
         } catch (error) {
             console.error("Error sending message:", error);
         }
@@ -44,17 +66,22 @@ const ChuckBot = () => {
         if (outputRef.current) {
             outputRef.current.scrollTop = outputRef.current.scrollHeight;
         }
-    }, [messages]);
+    }, [messages, isTyping]);
 
     return (
         <div>
             <div className={botOpen ? "ChuckBot" : "ChuckBot closed"}>
                 <div className="output-container" ref={outputRef}>
                     {messages.map((message, index) => (
-                        <div key={index} style={{ textAlign: message.mine ? "right" : "left" }}>
+                        <div key={index} className={message.mine ? "user-message" : "bot-message"} style={{ textAlign: message.mine ? "right" : "left" }}>
                             {message.text}
                         </div>
                     ))}
+                    {isTyping && (
+                        <div style={{ textAlign: "left", color: "rebeccapurple" }}>
+                            Chuck is typing...
+                        </div>
+                    )}
                 </div>
                 <div className="input-container">
                     <input
