4cac16d00e0a95641066c48c40855c4efac8b89e
K. Balázs <kutbal87@gmail.com>
2024-05-28 16:23:37 +0200

    Modify basic login method (adjust to recent changes), Add login method to return User data

--- MyCode Backend Server/MyCode Backend Server Tests/Services/TestLogin.cs
+++ MyCode Backend Server/MyCode Backend Server Tests/Services/TestLogin.cs
@@ -1,4 +1,5 @@
 ﻿using MyCode_Backend_Server.Contracts.Services;
+using MyCode_Backend_Server.Models;
 using Newtonsoft.Json;
 using System.Text;
 
@@ -6,16 +7,14 @@ namespace MyCode_Backend_Server_Tests.Services
 {
     public static class TestLogin
     {
-        public static async Task<(string authToken, IEnumerable<string> cookies, AuthResponse result)> Login_With_Test_User(AuthRequest request, HttpClient client)
+        public static async Task<(string authToken, IEnumerable<string> cookies, string result)> Login_With_Test_User(AuthRequest request, HttpClient client)
         {
             var authJsonRequest = JsonConvert.SerializeObject(request);
             var authContent = new StringContent(authJsonRequest, Encoding.UTF8, "application/json");
 
             var authResponse = await client.PostAsync("/users/login", authContent);
             authResponse.EnsureSuccessStatusCode();
-
-            var content = await authResponse.Content.ReadAsStringAsync();
-            var result = JsonConvert.DeserializeObject<AuthResponse>(content);
+            var result = await authResponse.Content.ReadAsStringAsync();
 
             var authToken = ExtractAuthTokenFromCookies(authResponse.Headers.GetValues("Set-Cookie"));
             var cookies = authResponse.Headers.GetValues("Set-Cookie");
@@ -23,6 +22,26 @@ namespace MyCode_Backend_Server_Tests.Services
             return (authToken, cookies, result!);
         }
 
+        public static async Task<User> Login_With_Test_User_Return_User(AuthRequest request, HttpClient client)
+        {
+            var (authToken, cookies, result) = await Login_With_Test_User(request, client);
+
+            if (result == null)
+                return null!;
+
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {authToken}");
+            foreach (var cookie in cookies)
+            {
+                client.DefaultRequestHeaders.Add("Cookie", cookie.Split(';')[0]);
+            }
+
+            var response = await client.GetAsync("/users/getUser");
+
+            var content = await response.Content.ReadAsStringAsync();
+            var returnedUser = JsonConvert.DeserializeObject<User>(content);
+
+            return returnedUser!;
+        }
 
         private static string ExtractAuthTokenFromCookies(IEnumerable<string> cookies)
         {
