eeac19db261449bf717370aec89459b6ea9ad4e4
K. Balázs <kutbal87@gmail.com>
2024-05-29 17:57:27 +0200

    Add tests to role status request and response

---
+++ MyCode Backend Server/MyCode Backend Server Tests/Models/RoleStatuses.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+using MyCode_Backend_Server.Contracts.Registers;
+using Assert = Xunit.Assert;
+
+namespace MyCode_Backend_Server_Tests.Models
+{
+    public class RoleStatusRequestTests
+    {
+        [Fact]
+        public void RoleStatusRequest_CanBeInitialized()
+        {
+            // Arrange
+            var status = "Active";
+
+            // Act
+            var roleStatusRequest = new RoleStatusRequest(status);
+
+            // Assert
+            Assert.Equal(status, roleStatusRequest.Status);
+        }
+
+        [Fact]
+        public void RoleStatusRequest_CanHandleNullStatus()
+        {
+            // Act
+            var roleStatusRequest = new RoleStatusRequest(null!);
+
+            // Assert
+            Assert.Null(roleStatusRequest.Status);
+        }
+
+        [Fact]
+        public void RoleStatusResponse_CanBeInitialized()
+        {
+            // Arrange
+            var status = "Active";
+
+            // Act
+            var roleStatusRequest = new RoleStatusResponse(status);
+
+            // Assert
+            Assert.Equal(status, roleStatusRequest.Status);
+        }
+
+        [Fact]
+        public void RoleStatusResponse_CanHandleNullStatus()
+        {
+            // Act
+            var roleStatusRequest = new RoleStatusResponse(null!);
+
+            // Assert
+            Assert.Null(roleStatusRequest.Status);
+        }
+    }
+}
