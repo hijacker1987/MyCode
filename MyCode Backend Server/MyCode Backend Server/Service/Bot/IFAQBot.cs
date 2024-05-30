affe600ba91d47d6cfe0ae2366e152f03299a3d2
K. Balázs <kutbal87@gmail.com>
2024-05-30 14:02:31 +0200

    Add interface for chat bot

---
+++ MyCode Backend Server/MyCode Backend Server/Service/Bot/IFAQBot.cs
@@ -0,0 +1,9 @@
+﻿using MyCode_Backend_Server.Models;
+
+namespace MyCode_Backend_Server.Service.Bot
+{
+    public interface IFAQBot
+    {
+        Task<string> OnMessageActivityAsync(BotMessage message);
+    }
+}
