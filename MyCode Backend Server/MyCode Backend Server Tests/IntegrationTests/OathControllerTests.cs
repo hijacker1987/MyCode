8e1e172e455b35a5d73998e1d40f5016a5bdabb7
K. Balázs <kutbal87@gmail.com>
2024-05-30 20:08:57 +0200

    Add new tests

---
+++ MyCode Backend Server/MyCode Backend Server Tests/IntegrationTests/OathControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using System.Net;
+using MyCode_Backend_Server;
+using Assert = Xunit.Assert;
+
+namespace MyCode_Backend_Server_Tests.IntegrationTests
+{
+    public class OathControllerTests(CustomWebApplicationFactory<Startup> factory) : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client = factory.CreateClient();
+
+        [Fact]
+        public async Task GoogleResponse_WithoutAuthentication_ReturnsBadRequest()
+        {
+            // Act
+            var response = await _client.GetAsync("/account/google-response");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task FacebookResponse_WithoutAuthentication_ReturnsBadRequest()
+        {
+            // Act
+            var response = await _client.GetAsync("/account/facebook-response");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GitHubResponse_WithoutAuthentication_ReturnsBadRequest()
+        {
+            // Act
+            var response = await _client.GetAsync("/account/github-response");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
